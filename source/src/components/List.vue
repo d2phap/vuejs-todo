f5cf35c0861c23133d9e1c4ad27e698666df47f7
Phap Duong <d2phap@gmail.com>
2018-10-06 18:41:51 +0800

    ListItem component: added vue functions: load item list, update item list, delete item, update item status

--- source/src/components/List.vue
+++ source/src/components/List.vue
@@ -1,9 +1,6 @@
 <template>
 	<ul class="list mt-3">
-        <ListItem />
-        <ListItem />
-        <ListItem />
-        <ListItem />
+        <ListItem v-for="item in itemList" :key="item.id" :text="item.text" :id="item.id" :isDone="item.isDone" @eventTaskStatusChange="onTaskStatusChange" @eventTaskDelete="onTaskDelete" />
     </ul>
 </template>
 
@@ -14,6 +11,87 @@
         name: "List",
         components: {
             ListItem
+        },
+        data() {
+            return {
+                itemList: [
+                    {
+                        id: 10,
+                        text: "Task 1",
+                        isDone: false
+                    },
+                    {
+                        id: 11,
+                        text: "Task 2",
+                        isDone: false
+                    },
+                    {
+                        id: 12,
+                        text: "Task 3",
+                        isDone: false
+                    },
+                ]
+            }
+        },
+        methods: {
+            /**
+             * Event: on task status changed
+             */
+            onTaskStatusChange(id, checked) {
+                console.log(id, checked)
+
+                let item = this.itemList.find(i => i.id == id)
+                if (item) {
+                    item.isDone = checked
+                }
+                
+                console.log(this.itemList)
+            },
+
+            /**
+             * Event: on task deleted
+             */
+            onTaskDelete(id) {
+                console.log(id)
+
+                let index = this.itemList.findIndex(i => i.id == id)
+                if (index > -1) {
+                    this.itemList.splice(index, 1)
+                }
+
+                console.log(this.itemList)
+            },
+
+            /**
+             * Load item list from local storage
+             */
+            loadItemList() {
+                this.itemList = JSON.parse(localStorage.getItem("VuejsTodo")) || []
+
+                console.log("this.itemList =", this.itemList)
+            },
+
+            /**
+             * Update the item list to local storage
+             */
+            updateItemList() {
+                localStorage.setItem("VuejsTodo", JSON.stringify(this.itemList))
+            }
+        },
+        mounted() {
+            // Load item list from local storage
+            this.loadItemList()
+        },
+        watch: {
+            itemList: {
+                handler(value) {
+                    console.log("item changed")
+
+                    // save to localStorage
+                    this.updateItemList()
+                },
+                deep: true
+            }
         }
 	}
 </script>
